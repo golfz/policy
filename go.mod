659a3e4c9c9fdbd60b4fd65226252f1c056e840c
surattikorn <surattikorn@gmail.com>
2023-01-31 20:06:00 +0700

    build: go get github.com/golang-jwt/jwt/v4

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mastertech-hq/authority
 
 go 1.19
 
-require github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+require github.com/golang-jwt/jwt/v4 v4.4.3
--- jwtutils/jwtutils.go
+++ jwtutils/jwtutils.go
@@ -1,7 +1,9 @@
 package jwtutils
 
 import (
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
+	"os"
+	"time"
 )
 
 type UserInterface interface {
@@ -9,15 +11,26 @@ type UserInterface interface {
 	UserType() string
 }
 
+type JWTData struct {
+	jwt.RegisteredClaims
+	CustomClaims map[string]string `json:"custom_claims"`
+}
+
 func GenerateToken(user UserInterface) (string, error) {
-	secret := "secret"
+	secret := os.Getenv("JWT_SECRET")
 
-	var token = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"iss":       "mastertime",
-		"uid":       user.UserID(),
-		"user-type": user.UserType(),
-	})
+	claims := JWTData{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+		},
+		CustomClaims: map[string]string{
+			"uid":       user.UserID(),
+			"user_type": user.UserType(),
+		},
+	}
 
+	var token = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	
 	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
 		return "", err
