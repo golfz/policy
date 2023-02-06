ab247190e27917b8bf4f7fcee50fb0b222ecf8f9
surattikorn <surattikorn@gmail.com>
2023-02-06 18:51:16 +0700

    feat: add parseClaims

--- application.go
+++ application.go
@@ -1,27 +1,49 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/mastertech-hq/authority/jwtutils"
+	"time"
 )
 
-type user struct {
-	uid      string
-	userType string
-}
-
-func (u *user) UserID() string {
-	return u.uid
-}
-
-func (u *user) UserType() string {
-	return u.userType
+type CustomClaims struct {
+	Uid       string `json:"uid"`
+	UserType  string `json:"user_type"`
+	TokenType string `json:"token_type"`
 }
 
 func main() {
-	u := &user{
-		uid:      "1234",
-		userType: "employee",
+	customClaims := CustomClaims{
+		Uid:       "d7dded03-833f-453a-9e26-860244a475bf",
+		UserType:  "employee",
+		TokenType: "access",
 	}
-	fmt.Println(jwtutils.GenerateToken(u))
+	token, err := jwtutils.GenerateToken(customClaims, 6*time.Second, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(token)
+
+	time.Sleep(2 * time.Second)
+
+	_, err = jwtutils.ParseToken(token)
+	if err != nil {
+		fmt.Println(err)
+		if errors.Is(err, jwt.ErrTokenExpired) {
+			fmt.Println(">>>> token expired")
+		}
+		return
+	}
+
+	us, err := jwtutils.GetCustomClaimString(token, "uid")
+	if err != nil {
+		fmt.Println(">>>> custom claims not ok", err)
+		return
+	}
+
+	fmt.Printf(">>>> claims: %+v\n", us)
+
 }
