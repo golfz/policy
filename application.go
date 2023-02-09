e5fa672d42d82687a8f3f9eb1b65b896c72215b0
surattikorn <surattikorn@gmail.com>
2023-02-09 17:17:46 +0700

    feat: change name for input, login form

--- application.go
+++ application.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"embed"
 	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/mastertech-hq/authority/jwtutils"
+	"html/template"
+	"net/http"
+	"os"
 	"time"
 )
+
+//type Page struct {
+//	Title string
+//	Body  []byte
+//}
+
+//go:embed web
+var webDir embed.FS
 
 type CustomClaims struct {
 	Uid       string `json:"uid"`
@@ -14,7 +26,41 @@ type CustomClaims struct {
 	TokenType string `json:"token_type"`
 }
 
+type LoginPageData struct {
+	LoginAction string
+}
+
 func main() {
+	http.HandleFunc("/oauth/v2/web/login", loginPage)
+	fmt.Println("ready.")
+	http.ListenAndServe(":8080", nil)
+}
+
+func loginPage(w http.ResponseWriter, r *http.Request) {
+	data := LoginPageData{
+		LoginAction: "/action/login",
+	}
+	printPage(w, "login.html", data)
+}
+
+func printPage(w http.ResponseWriter, fileName string, data interface{}) error {
+	//t, _ := template.ParseFiles(fmt.Sprintf("./web/%s", fileName))
+	t, err := template.ParseFS(webDir, "web/login.html")
+	if err != nil {
+		panic(err)
+	}
+	return t.Execute(w, data)
+}
+
+func loadPage(filename string) (string, error) {
+	b, err := os.ReadFile(fmt.Sprintf("./web/%s", filename))
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func PrintClaims() {
 	customClaims := CustomClaims{
 		Uid:       "d7dded03-833f-453a-9e26-860244a475bf",
 		UserType:  "employee",
@@ -45,5 +91,4 @@ func main() {
 	}
 
 	fmt.Printf(">>>> claims: %+v\n", us)
-
 }
