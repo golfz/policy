87f72bff98076b091c81de284cdb427e0f4f7fcd
surattikorn <surattikorn@gmail.com>
2023-02-17 05:25:58 +0700

    feat: validate Authorization is Bearer & get it

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
 	"net/http"
+	"strings"
 )
 
 func JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenString := r.Header.Get("Authorization")
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header is missing", http.StatusBadRequest)
+			return
+		}
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "Authorization header is not of type Bearer", http.StatusBadRequest)
+			return
+		}
+		tokenString := strings.Split(authHeader, " ")[1]
 		if tokenString == "" {
 			http.Error(w, "Authorization token is missing", http.StatusUnauthorized)
 			return
