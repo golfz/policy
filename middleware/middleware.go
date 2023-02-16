20678246e47527a8d6281aaa3a006f6921abee7e
surattikorn <surattikorn@gmail.com>
2023-02-17 05:22:13 +0700

    feat: add JWTMiddleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,29 @@
 package middleware
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"net/http"
+)
+
+func JWTMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			http.Error(w, "Authorization token is missing", http.StatusUnauthorized)
+			return
+		}
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte("secret"), nil // replace with your secret key
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid authorization token", http.StatusUnauthorized)
+			return
+		}
+		// Call the next handler, which can be another middleware in the chain, or the final handler
+		next.ServeHTTP(w, r)
+	})
+}
