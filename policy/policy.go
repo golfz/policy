4fc91d3dd85459499e1db14d4ea88e0901127e26
surattikorn <surattikorn@gmail.com>
2023-03-15 20:39:16 +0700

    feat: implement getStatementsForResource to pass testing

--- policy/policy.go
+++ policy/policy.go
@@ -6,7 +6,25 @@ func (p *Policy) IsAccessAllowed(r resources.Resource) (bool, error) {
 	return true, nil
 }
 
-func (p *Policy) GetStatementsForResource(res resources.Resource) ([]Statement, error) {
+func (p *Policy) getStatementsForResource(res resources.Resource) ([]Statement, error) {
+	var statements []Statement
 
-	return []Statement{}, nil
+	for _, stmt := range p.Statement {
+		if stmt.Resource != res.Resource {
+			continue
+		}
+		isContains := func(list []string, s string) bool {
+			for _, v := range list {
+				if v == s {
+					return true
+				}
+			}
+			return false
+		}
+		if isContains(stmt.Action, res.Action) {
+			statements = append(statements, stmt)
+		}
+	}
+
+	return statements, nil
 }
--- policy/policy_test.go
+++ policy/policy_test.go
@@ -1,6 +1,8 @@
 package policy
 
 import (
+	"github.com/mastertech-hq/authority/resources"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -8,6 +10,54 @@ func TestPolicy_IsAccessAllowed(t *testing.T) {
 
 }
 
-func TestPolicy_GetStatementsForResource(t *testing.T) {
+func TestPolicy_getStatementsForResource(t *testing.T) {
+	strPolicy := `
+	{
+		"Version": 1,
+		"PolicyID": "501228f3-f7f3-4ef1-8bc9-9fb73347f518",
+		"Statement": [	
+			{
+				"Effect": "Allow",
+				"Resource": "res:::leave",
+				"Action": [
+					"act:::leave:approve"
+				],
+				"Condition": {
+					"AtLeastOne": {
+						"StringIn": {
+							"prop:::employee:employee_uuid": [
+								"11111111",  
+                            	"22222222",  
+                            	"33333333",  
+                            	"44444444" 
+							]	
+						}
+					},  
+					"MustHaveAll": {  
+						"DateRange": {  
+							"sys:::now:date": {   
+								"to": "2023-01-31"  
+							}                    
+						}                
+					}  
+				}	
+			}
+		]
+	}`
+	p, err := ParseJSON([]byte(strPolicy))
+	assert.NoError(t, err)
 
+	res := resources.Resource{
+		Resource: "res:::leave",
+		Action:   "act:::leave:approve",
+		Properties: resources.Property{
+			String: map[string]string{
+				"prop:::employee:employee_uuid": "11111111",
+			},
+		},
+	}
+
+	statements, err := p.getStatementsForResource(res)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(statements))
 }
