cb7560c04f570cdd301a9192a7a07f5390f26caa
surattikorn <surattikorn@gmail.com>
2023-03-20 19:23:13 +0700

    test: add utils_test.go

---
+++ policy/utils_test.go
@@ -0,0 +1,51 @@
+package policy
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_isContainsInList(t *testing.T) {
+	// Test empty list
+	list := []int{}
+	s := 5
+	assert.False(t, isContainsInList(list, s))
+
+	// Test single-element list that contains s
+	list = []int{5}
+	assert.True(t, isContainsInList(list, s))
+
+	// Test single-element list that does not contain s
+	list = []int{10}
+	assert.False(t, isContainsInList(list, s))
+
+	// Test multiple-element list that contains s
+	list = []int{2, 4, 5, 7}
+	assert.True(t, isContainsInList(list, s))
+
+	// Test multiple-element list that does not contain s
+	list = []int{2, 4, 7, 8}
+	assert.False(t, isContainsInList(list, s))
+}
+
+func Test_isEquals(t *testing.T) {
+	// Test with equal integers
+	a := 5
+	b := 5
+	assert.True(t, isEquals(a, b))
+
+	// Test with unequal integers
+	a = 5
+	b = 10
+	assert.False(t, isEquals(a, b))
+
+	// Test with equal strings
+	c := "hello"
+	d := "hello"
+	assert.True(t, isEquals(c, d))
+
+	// Test with unequal strings
+	c = "hello"
+	d = "world"
+	assert.False(t, isEquals(c, d))
+}
