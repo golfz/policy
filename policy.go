555c3062575abce2480faf955a6c4b09c76ef71f
surattikorn <surattikorn@gmail.com>
2024-03-24 00:58:40 +0700

    feat: implement new spec

--- policy.go
+++ policy.go
@@ -1,22 +1,32 @@
 package policy
 
+import "fmt"
+
+const (
+	statementEffectAllow = "Allow"
+	statementEffectDeny  = "Deny"
+)
+
+const (
+	conditionMatched    = true
+	conditionNotMatched = false
+)
+
 type ResultEffect int
 
 const (
-	ignored ResultEffect = 0
+	DENIED  ResultEffect = 0
 	ALLOWED ResultEffect = 1
-	DENIED  ResultEffect = 2
 )
 
 // ----------------------------------------------
-// Policy
+// Policy struct
 // ----------------------------------------------
 
 type Policy struct {
 	Version    int
 	PolicyID   string
 	Statements []Statement
-	Err        error
 }
 
 type Statement struct {
@@ -27,57 +37,31 @@ type Statement struct {
 }
 
 type Condition struct {
-	AtLeastOne  map[string]Comparator `json:"AtLeastOne"`
-	MustHaveAll map[string]Comparator `json:"MustHaveAll"`
+	AtLeastOne  map[string]Comparator
+	MustHaveAll map[string]Comparator
 }
 
 type Comparator struct {
-	StringIn      *[]string  `json:"StringIn"`
-	StringEqual   *string    `json:"StringEqual"`
-	IntegerIn     *[]int     `json:"IntegerIn"`
-	IntegerEqual  *int       `json:"IntegerEqual"`
-	FloatIn       *[]float64 `json:"FloatIn"`
-	FloatEqual    *float64   `json:"FloatEqual"`
-	BooleanEqual  *bool      `json:"BooleanEqual"`
-	UserPropEqual *string    `json:"UserPropEqual"`
-	TimeRange     *string    `json:"TimeRange"`
-	DateRange     *string    `json:"DateRange"`
-	DateTimeRange *string    `json:"DateTimeRange"`
+	StringIn      *[]string
+	StringEqual   *string
+	IntegerIn     *[]int
+	IntegerEqual  *int
+	FloatIn       *[]float64
+	FloatEqual    *float64
+	BooleanEqual  *bool
+	UserPropEqual *string
+	//TimeRange     map[string]TimeRange
+	//DateRange     map[string]TimeRange
+	//DateTimeRange map[string]TimeRange
 }
 
-type StringCondition struct {
-	StringIn    map[string][]string `json:"StringIn,omitempty"`
-	StringEqual map[string]string   `json:"StringEqual,omitempty"`
-}
-
-type IntegerCondition struct {
-	IntegerIn    map[string][]int `json:"IntegerIn,omitempty"`
-	IntegerEqual map[string]int   `json:"IntegerEqual,omitempty"`
-}
-
-type FloatCondition struct {
-	FloatIn    map[string][]float64 `json:"FloatIn,omitempty"`
-	FloatEqual map[string]float64   `json:"FloatEqual,omitempty"`
-}
-
-type BooleanCondition struct {
-	BooleanIn    map[string][]bool `json:"BooleanIn,omitempty"`
-	BooleanEqual map[string]bool   `json:"BooleanEqual,omitempty"`
-}
-
-type TimeCondition struct {
-	TimeRange     map[string]TimeRange `json:"TimeRange,omitempty"`
-	DateRange     map[string]TimeRange `json:"DateRange,omitempty"`
-	DateTimeRange map[string]TimeRange `json:"DateTimeRange,omitempty"`
-}
-
-type TimeRange struct {
-	From string
-	To   string
-}
+//type TimeRange struct {
+//	From string
+//	To   string
+//}
 
 // ----------------------------------------------
-// Resource
+// Resource struct
 // ----------------------------------------------
 
 type Resource struct {
@@ -93,88 +77,203 @@ type Property struct {
 	Boolean map[string]bool
 }
 
+// ----------------------------------------------
+// interface
+// ----------------------------------------------
+
 type UserPropertyGetter interface {
-	GetUserProperty(prop string) (interface{}, bool)
+	GetUserProperty(prop string) string
 }
+
 type Validator interface {
 	IsAccessAllowed(res Resource) (ResultEffect, error)
 }
 
+// ----------------------------------------------
+// ValidationController
+// ----------------------------------------------
+
 type ValidationController struct {
 	Policies           []Policy
 	UserPropertyGetter UserPropertyGetter
-	err                error
+	Err                error
 }
 
-func (vc *ValidationController) IsAccessAllowed(res Resource) (ResultEffect, error) {
-	if vc.err != nil {
-		return DENIED, vc.err
+// IsAccessAllowed checks if the user is allowed to perform the action on the resource.
+func (ctrl *ValidationController) IsAccessAllowed(res Resource) (ResultEffect, error) {
+	if ctrl.Err != nil {
+		return DENIED, ctrl.Err
 	}
 
-	var results []ResultEffect
-
-	for _, p := range vc.Policies {
-		statements := p.getStatementsForResource(res)
-
-		// RULE 1:
-		// If there are no matched-statements, then the action is denied.
-		if len(statements) == 0 {
-			return DENIED, nil
-		}
-
-		var countAllow, countDeny uint
-
-		// Consider each statement
-		for _, stmt := range statements {
-			effect, err := considerStatement(stmt, res)
-			if err != nil {
-				return DENIED, err
-			}
-
-			switch effect {
-			case ALLOWED:
-				countAllow++
-			case DENIED:
-				countDeny++
-			}
-		}
-
-		// RULE 2:
-		// If found at least one statement with effect "Deny", then the action is "DENIED".
-		if countDeny > 0 {
-			return DENIED, nil
-		}
-
-		// RULE 3:
-		// If not found any statement with effect "Deny",
-		// and found at least one statement with effect "Allow",
-		// then the action is "ALLOWED".
-		if countAllow > 0 {
-			return ALLOWED, nil
-		}
-
-		// RULE 4:
-		// If not found any statement with effect "Deny" and "Allow",
-		// then the action is "DENIED".
-		results = append(results, DENIED)
+	var err error
+	statements := getMergedStatements(ctrl.Policies)
+	if err = ctrl.validateStatements(statements); err != nil {
+		return DENIED, err
+	}
+	statements, err = ctrl.filterMatchedStatements(statements, res)
+	if err != nil {
+		return DENIED, err
 	}
 
-	// TODO: Implement the logic for combining the results from multiple policies
-	return DENIED, nil
+	// Rule 1: If there are no matching statements, then the result is "DENIED".
+	if len(statements) == 0 {
+		return DENIED, nil
+	}
+
+	// Rule 2: If there is at least one "Deny" statement, then the result is "DENIED".
+	for _, stmt := range statements {
+		if stmt.Effect == statementEffectDeny {
+			return DENIED, nil
+		}
+	}
+
+	// Rule 3: If all statements are "Allow" statements, then the result is "ALLOWED".
+	return ALLOWED, nil
 }
 
-func (p Policy) getStatementsForResource(res Resource) []Statement {
-	var statements []Statement
+// validateStatements function checks if the effect of each statement is valid.
+// If the effect is not 'Allow' or 'Deny', it returns an error.
+func (ctrl *ValidationController) validateStatements(statements []Statement) error {
+	for _, stmt := range statements {
+		if !isValidEffect(stmt.Effect) {
+			return fmt.Errorf("invalid effect: %s", stmt.Effect)
+		}
+	}
+	return nil
+}
 
-	for _, stmt := range p.Statements {
+func (ctrl *ValidationController) filterMatchedStatements(statements []Statement, res Resource) ([]Statement, error) {
+	var filteredStatements []Statement
+	for _, stmt := range statements {
+		// filter by resource and action
 		if stmt.Resource != res.Resource {
 			continue
 		}
-		// If match with resource, then check action
-		if isContainsInList(stmt.Action, res.Action) {
-			statements = append(statements, stmt)
+		if !isContainsInList(stmt.Actions, res.Action) {
+			continue
+		}
+
+		// Rule 4: If there are no conditions, then the statement is considered matched.
+		if stmt.Conditions == nil {
+			filteredStatements = append(filteredStatements, stmt)
+			continue
+		}
+
+		isMatched, err := ctrl.considerStatementConditions(*stmt.Conditions, res)
+		if err != nil {
+			return nil, err
+		}
+		if isMatched {
+			filteredStatements = append(filteredStatements, stmt)
+		}
+	}
+	return filteredStatements, nil
+}
+
+func (ctrl *ValidationController) considerStatementConditions(condition Condition, res Resource) (bool, error) {
+	isAtLeastOneConditionMatched, err := ctrl.considerAtLeastOneCondition(condition.AtLeastOne, res)
+	if err != nil {
+		return conditionNotMatched, err
+	}
+
+	isMustHaveAllConditionMatched, err := ctrl.considerMustHaveAllCondition(condition.MustHaveAll, res)
+	if err != nil {
+		return conditionNotMatched, err
+	}
+
+	isMatched := isAtLeastOneConditionMatched && isMustHaveAllConditionMatched
+	return isMatched, nil
+}
+
+func (ctrl *ValidationController) considerAtLeastOneCondition(conditions map[string]Comparator, res Resource) (bool, error) {
+	matched, total, err := ctrl.countMatchedConditions(conditions, res)
+	if err != nil {
+		return conditionNotMatched, err
+	}
+	if total == 0 {
+		return conditionMatched, nil
+	}
+	return matched > 0, nil
+}
+
+func (ctrl *ValidationController) considerMustHaveAllCondition(conditions map[string]Comparator, res Resource) (bool, error) {
+	matched, total, err := ctrl.countMatchedConditions(conditions, res)
+	if err != nil {
+		return conditionNotMatched, err
+	}
+	if total == 0 {
+		return conditionMatched, nil
+	}
+	return matched == total, nil
+}
+
+func (ctrl *ValidationController) countMatchedConditions(conditions map[string]Comparator, res Resource) (matched, total int, err error) {
+	total = len(conditions)
+	for valueRefKey, comparator := range conditions {
+		if ctrl.isMatchedComparator(comparator, res.Properties, valueRefKey) {
+			matched++
+		}
+	}
+	return
+}
+
+func (ctrl *ValidationController) isMatchedComparator(comparator Comparator, prop Property, valueRefKey string) bool {
+	if comparator.StringIn != nil {
+		if !isContainsInList(*comparator.StringIn, prop.String[valueRefKey]) {
+			return false
+		}
+	}
+	if comparator.StringEqual != nil {
+		if *comparator.StringEqual != prop.String[valueRefKey] {
+			return false
+		}
+	}
+	if comparator.IntegerIn != nil {
+		if !isContainsInList(*comparator.IntegerIn, prop.Integer[valueRefKey]) {
+			return false
+		}
+	}
+	if comparator.IntegerEqual != nil {
+		if *comparator.IntegerEqual != prop.Integer[valueRefKey] {
+			return false
+		}
+	}
+	if comparator.FloatIn != nil {
+		if !isContainsInList(*comparator.FloatIn, prop.Float[valueRefKey]) {
+			return false
+		}
+	}
+	if comparator.FloatEqual != nil {
+		if *comparator.FloatEqual != prop.Float[valueRefKey] {
+			return false
+		}
+	}
+	if comparator.BooleanEqual != nil {
+		if *comparator.BooleanEqual != prop.Boolean[valueRefKey] {
+			return false
+		}
+	}
+	if comparator.UserPropEqual != nil {
+		if ctrl.UserPropertyGetter.GetUserProperty(*comparator.UserPropEqual) != prop.String[valueRefKey] {
+			return false
 		}
 	}
 
+	return true
+}
+
+// ----------------------------------------------
+// Helper functions
+// ----------------------------------------------
+
+func getMergedStatements(policies []Policy) []Statement {
+	var statements []Statement
+	for _, policy := range policies {
+		statements = append(statements, policy.Statements...)
+	}
 	return statements
+}
+
+func isValidEffect(effect string) bool {
+	return effect == statementEffectAllow || effect == statementEffectDeny
 }
